a31563bec0a5b6abca80e4b6537bc761f84c536c
noyoncodeware <noyon.codeware@gmail.com>
2024-07-28 20:28:48 +0600

    image buffer issue

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const morgan = require("morgan");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {
+  createProxyMiddleware,
+  responseInterceptor,
+} = require("http-proxy-middleware");
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const API_SERVICE_URL = "https://graph.facebook.com";
 
 app.use(morgan("dev"));
+
+const bufferProxyMiddleware = createProxyMiddleware({
+  changeOrigin: true,
+  router: (req) => req.query.__host__,
+  selfHandleResponse: true,
+  on: {
+    proxyRes: (proxyRes, req, res) => {
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      proxyRes.pipe(res);
+    },
+  },
+});
+
+app.use("/buffer", bufferProxyMiddleware);
+app.use(
+  "/common",
+  createProxyMiddleware({
+    changeOrigin: true,
+    router: (req) => req.query.__host__,
+  })
+);
+
 app.use(
   "/api",
   createProxyMiddleware({
