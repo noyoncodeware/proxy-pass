cae2f3381ee2c0448a76e6ce0b3200dad031c8ab
noyoncodeware <noyon.codeware@gmail.com>
2024-07-31 15:01:38 +0600

    fix: headers issue

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-const {
-  createProxyMiddleware,
-  responseInterceptor,
-} = require("http-proxy-middleware");
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const http = require("http");
+const https = require("https");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -25,13 +23,40 @@ const bufferProxyMiddleware = createProxyMiddleware({
 });
 
 app.use("/buffer", bufferProxyMiddleware);
-app.use(
-  "/common",
-  createProxyMiddleware({
-    changeOrigin: true,
-    router: (req) => req.query.__host__,
-  })
-);
+
+const customProxyMiddleware = (req, res, next) => {
+  const targetUrl = req.query.__host__;
+  if (!targetUrl) {
+    return res
+      .status(400)
+      .json({ error: "__host__ query parameter is required" });
+  }
+  const parsedUrl = new URL(targetUrl);
+  const protocol = parsedUrl.protocol === "https:" ? https : http;
+  const options = {
+    hostname: parsedUrl.hostname,
+    //port: parsedUrl.port || (parsedUrl.protocol === "https:" ? 443 : 80),
+    path: targetUrl + req.url.replace("/common", ""),
+    method: req.method,
+    headers: req.headers,
+  };
+  const proxyReq = protocol.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, {
+      end: true,
+    });
+  });
+
+  proxyReq.on("error", (err) => {
+    res.status(500).json({ error: "Proxy Error", details: err.message });
+  });
+
+  req.pipe(proxyReq, {
+    end: true,
+  });
+};
+
+app.use("/common", customProxyMiddleware);
 
 app.use(
   "/api",
