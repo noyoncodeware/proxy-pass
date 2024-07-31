d6c206f8f17d673207c3eea963d1e3dc34d54eb5
noyoncodeware <noyon.codeware@gmail.com>
2024-07-31 19:11:02 +0600

    fix: headers issue

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const customProxyMiddleware = async (request, response) => {
       responseType: "stream",
       headers: {
         "User-Agent": getRandomUserAgent(),
+        Referer: "https://www.facebook.com/",
       },
     });
     arrayBuffer.data.pipe(response);
